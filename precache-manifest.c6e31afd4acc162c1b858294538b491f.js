51182822a825c561848487bd903989c1b2a8a4da
Nichole Spencer <nic_spencer@hotmail.co.uk>
2020-05-10 15:36:28 +0100

    Updates

---
+++ precache-manifest.c6e31afd4acc162c1b858294538b491f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf75655dc9514d6215f54d5bde5b34e",
+    "url": "/todo_react_application/index.html"
+  },
+  {
+    "revision": "243e3b98488d2874ff58",
+    "url": "/todo_react_application/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5d35223c0dbf92071c6f",
+    "url": "/todo_react_application/static/css/main.02423cde.chunk.css"
+  },
+  {
+    "revision": "243e3b98488d2874ff58",
+    "url": "/todo_react_application/static/js/2.2165c7fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_react_application/static/js/2.2165c7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d35223c0dbf92071c6f",
+    "url": "/todo_react_application/static/js/main.70387745.chunk.js"
+  },
+  {
+    "revision": "5d4c1a4e2e9a73a8502e",
+    "url": "/todo_react_application/static/js/runtime-main.606d0e57.js"
+  }
+]);
